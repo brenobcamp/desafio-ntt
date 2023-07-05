c60ab341446dc6a455f7592a6d4dc6784ee57435
Breno Batista Campos <brenobatistacampos@gmail.com>
2023-07-05 17:55:44 +0000

    agora fazer o front

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ from bson import json_util
 from werkzeug.utils import secure_filename
 import json
 import os
+from pydantic import BaseModel
+from http import HTTPStatus
+from flask_pydantic import validate
+
+
+class Code(BaseModel):
+    code: str
+    url: str
+    image: str
+
 
 app = Flask(__name__)
 uri = "mongodb+srv://brenocampos:sapoazul@clusterazure.tybruw9.mongodb.net/?retryWrites=true&w=majority"
@@ -12,36 +22,69 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 db = client.urls
 collection = db.urls
 
+
+# Web
+
 @app.route("/")
 def home():
     return render_template("home.html", documentos=collection.find())
+
 
 @app.route("/post", methods=['GET', 'POST'])
 def post():
     if request.method == 'POST':
         file = request.files['file']
-        full_name = request.form['code'] + secure_filename(file.filename)
+        full_name = request.form['nome_cientifico'] + secure_filename(file.filename)
         file.save('/workspace/desafio-ntt/static/user_files/' + full_name)
 
         post = {
-            'code': request.form['code'].strip(),
-            'url': request.form['url'].strip(),
-            'image': 'https://5000-brenobcamp-desafiontt-1j9w3kam4lw.ws-us101.gitpod.io/static/user_files/' + full_name
+            'nome_comum': request.form['nome_comum'].strip(),
+            'nome_cientifico': request.form['nome_cientifico'].strip(),
+            'ordem': request.form['ordem'].strip(),
+            'imagem': 'https://5000-brenobcamp-desafiontt-1j9w3kam4lw.ws-us101.gitpod.io/static/user_files/' + full_name
         }
         collection.insert_one(post)
         
         return "Created"
 
+
 @app.route("/delete", methods=['POST', 'GET'])
 def delete():
     if request.method == 'POST':
-        if collection.find_one({"code": request.form['delete']}):
-            delete = collection.find_one({"code": request.form['delete']})
-            os.remove(f'/workspace/desafio-ntt/static/user_files/{delete["image"]}')
-            collection.delete_one({'code': request.form['delete']})
+        if collection.find_one({"nome_comum": request.form['delete']}):
+            delete = collection.find_one({"nome_comum": request.form['delete']})
+            file = delete['imagem'].split("https://5000-brenobcamp-desafiontt-1j9w3kam4lw.ws-us101.gitpod.io/static/user_files/")
+            file = file[1]
+            os.remove(f'/workspace/desafio-ntt/static/user_files/{file}')
+            collection.delete_one({'nome_comum': request.form['delete']})
             return "Deleted"
+    return abort(404)
 
-@app.route("/del/<code>", methods=["GET", "DELETE"])
+
+@app.route("/get", methods=['GET'])
+def get():
+    find = collection.find()
+    list_cur = list(find)
+    json = json_util.dumps(list_cur)
+    return json
+
+# API
+
+@app.route("/create", methods=["POST"])
+@validate()
+def create():
+    collection.insert_one(request.json)
+    return jsonify({"code": HTTPStatus.OK,
+            "message": "Registro criado com sucesso"})
+
+
+@app.route("/update/<string:code>", methods=['PUT'])
+def put(code, url, image):
+    collection.update_one({'code': code, 'url': url, 'image': image})
+    return 'Updated', 200
+
+
+@app.route("/del/<string:code>", methods=["DELETE"])
 def delete_code(code):
     if collection.find_one({"code": code}):
         delete = collection.find_one({"code": code})
@@ -52,12 +95,6 @@ def delete_code(code):
         return 'Deleted', 200
     return abort(404)
 
-@app.route("/get", methods=['GET', 'POST'])
-def get():
-    find = collection.find()
-    list_cur = list(find)
-    json = json_util.dumps(list_cur)
-    return json
 
 @app.route("/<string:code>")
 def redirect_to_url(code):
@@ -65,6 +102,7 @@ def redirect_to_url(code):
         document = collection.find_one({"code": code})
         return redirect(url_for('static', filename='user_files/' + document['image']))
     return abort(404)
+
 
 @app.errorhandler(404)
 def page_not_found(error):
